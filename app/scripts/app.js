707b49f7d90eb932624766419583b5da7111fc64
Mohnish Saini <sn.mohnish@gmail.com>
2021-01-24 17:32:32 +0800

    Second Commit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,20 +17,63 @@ app.config(function($routeProvider) {
         templateUrl : "../app/pages/kycwriteup.html"
     }).when("/rmTagging", {
         templateUrl : "../app/pages/rmTagging.html"
+    }).when("/riskAssessment", {
+        templateUrl : "../app/pages/riskAssessment.html"
     });
 });
 
-app.controller("myCtrl", function($scope) {
+app.controller("myCtrl", function($scope, $http, $location) {
+
+/*
+    $http.get("app/json/entity.json")
+    .then(function(response) {
+        
+        var myObj = JSON.parse(response);
+        $scope.entityValues = myObj.values;
+    }), function myError(response) {
+        $scope.myWelcome = response.statusText;
+      });
+*/
+      $http({
+        method : "GET",
+          url : "app/json/entity.json"
+      }).then(function mySuccess(response) {
+        $scope.entityValues = response.data.values;
+      }, function myError(response) {
+        // error handling
+      });
+
+      $scope.countryList = [];
+      $http({
+        method : "GET",
+          url : "app/json/countries.json"
+      }).then(function mySuccess(response) {
+
+        response.data.forEach(function(obj){
+            $scope.countryList.push(obj.name);
+        });
+      }, function myError(response) {
+        // error handling
+      });
+
+    //$scope.callJson();
 
     //alert('111');
-    $scope.selectEntity = "Entity A";
-    $scope.selectRelManager = "Manager A";
-    $scope.selectRMSalesCode = "RM Sales Code A";
-    $scope.selectLMName = "LM Name A";
-    $scope.selectAccountType = "Account Type A";
 
-    var entityDrpDwnValues = '{ "values": [ "Entity A", "Entity B", "Entity C", "Entity D", "Entity E"]}';
-    $scope.entityValues = JSON.parse(entityDrpDwnValues);
+    $scope.rmtagging = {};
+    
+
+    $scope.rmtagging.entity = "Entity A";
+    $scope.rmtagging.relManager = "Manager A";
+    $scope.rmtagging.rmSalesCode = "RM Sales Code A";
+    $scope.rmtagging.lmName = "LM Name A";
+    $scope.rmtagging.accountType = "Account Type A";
+
+    $scope.show = false;
+    $scope.showKycWriteupJson = false;
+
+    //var entityDrpDwnValues = '{ "values": [ "Entity A", "Entity B", "Entity C", "Entity D", "Entity E"]}';
+    //$scope.entityValues = JSON.parse(entityDrpDwnValues);
 
     var relManagerNameDrpDwnValues = '{ "values": [ "Manager A", "Manager B", "Manager C", "Manager D", "Manager E"]}';
     $scope.relManagerNameValues = JSON.parse(relManagerNameDrpDwnValues);
@@ -46,23 +89,30 @@ app.controller("myCtrl", function($scope) {
 
 
     $scope.rmTaggingSave = function () {
-        alert('rmTaggingSave');
-      }
-
-
-    $scope.addItem = function () {
-      $scope.errortext = "";
-      if (!$scope.addMe) {return;}
-      if ($scope.products.indexOf($scope.addMe) == -1) {
-        $scope.products.push($scope.addMe);
-      } else {
-        $scope.errortext = "The item is already in your shopping list.";
-      }
+        $scope.show = !$scope.show;
     }
-    $scope.removeItem = function (x) {
-      $scope.errortext = "";
-      $scope.products.splice(x, 1);
-    } 
+
+    $scope.rmTaggingNext = function () {
+        $location.path('/kycwriteup'); 
+    }
+
+    $scope.kycWriteupPrevious = function () {
+        $location.path('/rmTagging'); 
+    }
+
+    $scope.kycWriteupSaveAsDraft = function () {
+        $scope.showKycWriteupJson = !$scope.showKycWriteupJson;
+    }
+
+    $scope.kycWriteupSaveAndNext = function () {
+        $scope.show = !$scope.show;
+        $location.path('/riskAssessment'); 
+    }
+
+    $scope.kycwriteup = {};
+
+
+    
   });
 
 
